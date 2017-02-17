97f1f56958e4cf3d4ef012544956139744d1b283
mfdyyge <mfdyyge@qq.com>
2017-02-17 17:35:03 +0800

    初始导入

---
+++ springmvc/src/main/java/com/mark/java/service/UserService.java
@@ -0,0 +1,10 @@
+package com.mark.java.service;
+
+import java.util.List;
+
+/**
+ * Created by 小觜冰凉 on 2017-2-6 0006.
+ */
+public interface UserService {
+    public List<String> getAllUsernames();
+}
